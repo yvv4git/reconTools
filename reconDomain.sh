22d1f7c078d5855a96087a3bb07e4d47299d38e4
yvv4git <yvv4git@mail.ru>
2019-12-07 14:14:09 +0300

    Modify

--- reconDomain.sh
+++ reconDomain.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# ./recon.sh  domain.ru
+# USE: ./recon.sh  domain.ru
 . ./src/fDirCreate.sh
 . ./src/fReconDns.sh
 . ./src/fReconWhois.sh
@@ -20,7 +20,7 @@ route=$(ReconWhois $ipv4 $workPath)
 ReconDns $targetDomain $workPath
 
 # SCAN NMAP
-proxychains nmap -sV -T4 --dns-servers 8.8.8.8 -v -iL $workPath'/hosts_ipv4.lst' -oA $workPath'/scan_nmap'
+proxychains nmap -sV -T5 --dns-servers 8.8.8.8 -v -iL $workPath'/hosts_ipv4.lst' -oA $workPath'/scan_nmap'
 
 # ANALYSE
 searchsploit --nmap $workPath'/scan_nmap.xml' > $workPath'/anaylyse_searchsploit.txt'
--- src/fReconDns.sh
+++ src/fReconDns.sh
@@ -1,5 +1,5 @@
 function ReconDns() {
-    dnsrecon -d $1 -asgw --xml $2'/dns_dnsrecon.xml';
+    dnsrecon -d $1 -n 8.8.8.8 -asgw --xml $2'/dns_dnsrecon.xml';
     grep -oP "([0-9]{1,3}\.){3}[0-9]{1,3}" $2'/dns_dnsrecon.xml' >> $2'/hosts_ipv4.lst';
     sort -t . -k 3,3n -k 4,4n $2'/hosts_ipv4.lst' >> $2'/hosts_ipv4.lst'
 }
